7a5c01b720adcfab2899c24bbcd7057fe96109f3
近並 樹 <aika@kinnaminombp.lan>
2021-01-01 19:40:20 +0900

    [addtion] add sample code

---
+++ function/src/main.py
@@ -0,0 +1,17 @@
+import pytest
+class test_util():
+  def addition(self, num1: int, num2: int) -> int:
+    return num1 + num2
+
+  def greeting(self, name: str) -> str:
+    return 'Hello, ' + name
+
+
+
+def test_addition():
+  target = test_util()
+  assert target.addition(1, 2) == 3
+
+def test_greeting():
+  target = test_util()
+  assert target.greeting('aika') == 'Hello, aika'
